f0c89af0071db41918f2cfb5d73f148616f0c798
arie <arieinjector@gmail.com>
2022-09-23 19:58:02 +0700

    fixed cc

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,7 +26,7 @@
               <label for="floatingPassword">Password</label>
             </div>
             <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-          <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+          <p class="mt-5 mb-3 text-muted">&copy; 2022-2023</p>
         </form>
       </main>
 
